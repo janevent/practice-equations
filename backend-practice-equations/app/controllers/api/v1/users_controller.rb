c735f32cf60aef23aabb07c818085aaf1fd214c0
Jane Venter <janeanneventer@gmail.com>
2019-12-18 11:48:10 -0800

    set up files, and create a users table, model and controller in rails

---
+++ backend-practice-equations/app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,35 @@
+class Api::V1::UsersController < ApplicationController
+
+  def index 
+    @users = User.all
+    render json: @users, status: 200
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user, status: 200
+  end
+
+  def create
+    @user = User.create(user_params)
+    render json: @user, status: 200
+  end
+
+  def update 
+    @user = User.find(params[:id])
+    @user.update(user_params)
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.delete
+
+    render json: {userId: @user.id}
+  end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:name, :password_digest)
+  end
+end
---
+++ backend-practice-equations/config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  #get 'users/name'
+  #get 'users/password_digest'
+namespace :api do 
+  namespace :v1 do
+    resources :users
+  end
+end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
