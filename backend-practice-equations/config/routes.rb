a32521e3ad3974d9f23553efc0616ee667e956b7
Jane Venter <janeanneventer@gmail.com>
2019-12-20 16:23:21 -0800

    adding devise authentication

--- backend-practice-equations/config/routes.rb
+++ backend-practice-equations/config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
   #get 'users/name'
   #get 'users/password_digest'
-namespace :api do 
-  namespace :v1 do
-    resources :users
-  end
+#namespace :api do 
+ # namespace :v1 do
+  #  resources :users
+  #end
+#end
+scope :api, defaults: { format: :json } do
+  devise_for :users 
 end
+#defaults: { format: :json }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
